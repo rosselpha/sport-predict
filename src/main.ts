ea21ed87f62b044cd3dda1b427c2a47d44389240
ross elpharin <elpharin@gmail.com>
2023-02-21 09:04:16 -0500

    port update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT;
+  const port = process.env.PORT ||8081;
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
